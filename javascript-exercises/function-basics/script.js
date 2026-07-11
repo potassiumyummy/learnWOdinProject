550fb917ceb3174c74fadd0f4e6c3b8f32aea036
pottassiumyummy <cutypiecutie@gmail.com>
2026-07-12 01:56:05 +0800

    final commit for function-basics

--- javascript-exercises/function-basics/script.js
+++ javascript-exercises/function-basics/script.js
@@ -1,5 +1,25 @@
 function add7(num){
-    return num + 7;
+    num = num + 7;
+    return num
 }
 
-console.log(add7(10));   
+console.log(add7(10));
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiply(3,2));
+
+function capitalize(word){
+    let capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+    return capitalized;
+}
+
+console.log(capitalize("mommy"));
+
+function lastletter(text){
+    return text.at(-1);
+}
+
+console.log(lastletter("NeilAndrie"));
