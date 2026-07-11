fcce7e066ea2d6035b26e6b11d430469c58340aa
pottassiumyummy <cutypiecutie@gmail.com>
2026-07-11 23:07:54 +0800

    post-repeatString activity commit

--- javascript-exercises/foundations/06_repeatString/repeatString.js
+++ javascript-exercises/foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(input, rotation) {
+    if(rotation < 0){
+        return "ERROR";
+    }else{
+        let string = "";
+        for(let i = 0; i < rotation; i++){
+            string += input;
+        }
+        return string;
+    }
 };
 
 // Do not edit below this line
