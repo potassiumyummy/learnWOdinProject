4cf29c9d34257745f0cdc720a0affa9ae99bf3af
jabrrtbrrt <cutypiecutie@gmail.com>
2026-07-21 20:57:51 +0800

    added sumAll exercise

--- javascript-exercises/foundations/09_sumAll/sumAll.js
+++ javascript-exercises/foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    let sum = 0;
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    
+
+    return sum;
 };
 
 // Do not edit below this line
