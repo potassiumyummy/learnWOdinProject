7114b4da3304bb641b9f132ec1af591bf774bbb9
jabrrtbrrt <cutypiecutie@gmail.com>
2026-07-21 22:09:44 +0800

    added leapYears and tempConversion exercises

--- javascript-exercises/foundations/11_tempConversion/tempConversion.js
+++ javascript-exercises/foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  const cel =  (farenheit - 32) * (5 / 9);
+  return Number(cel.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const far = (celcius * (9 / 5)) + 32;
+  return Number(far.toFixed(1));
 };
 
 // Do not edit below this line
