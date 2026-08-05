965922e96b2747497cbec1c60fa86e966aef963e
jabrrtbrrt <cutypiecutie@gmail.com>
2026-08-05 22:40:36 +0800

    palindromes test PASSED

--- javascript-exercises/foundations/13_palindromes/palindromes.js
+++ javascript-exercises/foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleaned = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleaned === cleaned.split('').reverse().join('');
 };
 
 // Do not edit below this line
