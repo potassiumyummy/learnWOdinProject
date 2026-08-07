a68a88fdb422195e1ddb4d6dd70d75a1ed2a612f
jabrrtbrrt <cutypiecutie@gmail.com>
2026-08-07 18:49:35 +0800

    findTheOldest added

--- javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
+++ javascript-exercises/foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    const age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+    return age > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
