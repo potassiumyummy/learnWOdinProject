e3158f94f843ecb7295f98269f32693339157246
jabrrtbrrt <cutypiecutie@gmail.com>
2026-08-07 20:15:46 +0800

    initial commit for odinCalculator

---
+++ javascript-exercises/foundations/odinCalculator/script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector("container");
+
+function add(num1, num2){
+    return num1 + num2;
+}
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function divide(num1, num2){
+    return num1/num2;
+}
+
+const operate = (num1, num2, operator) => {
+    switch(operator){
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+};
+
